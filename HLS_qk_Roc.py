0517c9b0b4e311f62ef018c32fae1dce89d0db4a
Javier Duarte <jduarte@ucsd.edu>
2023-12-20 17:57:23 -0800

    update profiling

--- HLS_qk_Roc.py
+++ HLS_qk_Roc.py
@@ -20,6 +20,8 @@ import hls4ml
 config = hls4ml.utils.config_from_keras_model(model, granularity='name')
 print("-----------------------------------")
 
+for layer in config['LayerName']:
+    config['LayerName'][layer]['Trace'] = True
 config['LayerName']['q_conv1d']['ReuseFactor'] = 2
 config['LayerName']['q_conv1d_1']['ReuseFactor'] = 2
 config["LayerName"]["q_conv1d"]["ConvImplementation"] = "Pointwise"
@@ -28,16 +30,16 @@ config["LayerName"]["input_1"]["Precision"] = "fixed<10,3>"
 
 hls_model = hls4ml.converters.convert_from_keras_model(model,
                                                        hls_config=config,
-                                                       output_dir='qkmodel_1/hls4ml_prj',
+                                                       output_dir='hls4ml_prj',
                                                        part='xcvu13p-flga2577-2-e')
 
 #Compile model, no need to convert if we are plotting performance
 hls_model.compile()
 
 # Handle Data:
-with h5py.File("/home/rmarroqu/L1JetTagDaniel/dataForgeScripts/newTestDataST30.h5", "r") as hf:
+with h5py.File("/home/users/russelld/L1JetTagger/dataForgeScripts/newTestDataST30.h5", "r") as hf:
     dataset = hf["Testing Data"][:]
-with h5py.File("/home/rmarroqu/L1JetTagDaniel/dataForgeScripts/testingDataQCD30.h5", "r") as hf:
+with h5py.File("/home/users/russelld/L1JetTagger/dataForgeScripts/testingDataQCD30.h5", "r") as hf:
     datasetQCD = hf["Testing Data"][:]
     
 dataset = np.concatenate((dataset,datasetQCD)) #Stacking datasets on top of another
@@ -66,6 +68,39 @@ fpr_Ab_hls, tpr_Ab_hls, thresholds_Ab_hls = roc_curve(b, Ab_pred_hls_qkeras)
 auc_Ab_hls = auc(fpr_Ab_hls, tpr_Ab_hls)
 
 
+plt.figure()
+plt.hist(Ab_pred_qkeras.flatten(), weights=b.flatten(), bins=np.linspace(0, 1, 100), label='QKeras signal', alpha=0.7)
+plt.hist(Ab_pred_qkeras.flatten(), weights=(1-b).flatten(), bins=np.linspace(0, 1, 100), label='QKeras bkgd', alpha=0.7)
+plt.hist(Ab_pred_hls_qkeras.flatten(), weights=b.flatten(), bins=np.linspace(0, 1, 100),label='HLS signal', alpha=0.7)
+plt.hist(Ab_pred_hls_qkeras.flatten(), weights=(1-b).flatten(), bins=np.linspace(0, 1, 100),label='HLS bkg', alpha=0.7)
+plt.legend()
+plt.savefig("dist.png", dpi=300)
+
+plt.figure()
+plt.scatter(Ab_pred_hls_qkeras.flatten(), Ab_pred_qkeras.flatten(), s=0.2)
+min_x = 0
+max_x = 1
+plt.plot([min_x, max_x], [min_x, max_x], c='gray')
+plt.xlabel('hls4ml')
+plt.ylabel('QKeras')
+plt.savefig("output_correlation.png", dpi=300)
+
+
+y_hls, hls4ml_trace = hls_model.trace(X_test)
+keras_trace = hls4ml.model.profiling.get_ymodel_keras(model, X_test)
+
+for layer in hls4ml_trace.keys():
+    plt.figure()
+    if layer not in keras_trace: continue
+    plt.scatter(hls4ml_trace[layer].flatten(), keras_trace[layer].flatten(), s=0.2)
+    min_x = min(np.amin(hls4ml_trace[layer]), np.amin(keras_trace[layer]))
+    max_x = max(np.amax(hls4ml_trace[layer]), np.amax(keras_trace[layer]))
+    plt.plot([min_x, max_x], [min_x, max_x], c='gray')
+    plt.xlabel(f'hls4ml {layer}')
+    plt.ylabel(f'QKeras {layer}')
+    plt.savefig(f'profiling_{layer}.png', dpi=300)
+
+plt.figure()
 plt.plot(fpr_Ab_qkeras, tpr_Ab_qkeras, label=" qkeras AUC={:.3f}".format(auc_Ab_qkeras))
 plt.plot(fpr_Ab_hls, tpr_Ab_hls, "--" ,label=" HLS AUC={:.3f}".format(auc_Ab_hls))
 
@@ -77,4 +112,7 @@ plt.title("L1 LLP Tag Qk Model ROC Curve", fontsize=16, weight="bold")
 plt.legend(loc="best")
 plt.xscale("log")
 plt.grid(True)
-plt.savefig("HLS_qk_ROCCurve.png")
+plt.savefig("hls_Qk_ROCCurve.png")
+
+hls4ml.utils.plot_model(hls_model, show_shapes=True, show_precision=True, to_file='model_summary.png')
+
